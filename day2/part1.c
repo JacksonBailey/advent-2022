3087135b7dd5759f791a70a599d6c4c064d9a37d
Jackson Bailey <jbailey@happymoney.com>
2022-12-16 14:28:00 -0500

    Troubleshooting

--- day2/part1.c
+++ day2/part1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 // https://adventofcode.com/2022/day/2
 
@@ -20,9 +22,17 @@ FILE *open(int argc, char *argv[])
 	return fp;
 }
 
+// a = rock, b = paper, c = scissors
+// x = rock, y = paper, z = scissors
+
 // core for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
 // plus the score for the outcome of the round (0 if you lost, 3 if the round
 // was a draw, and 6 if you won).
+
+bool compare(const char *s1, const char *s2)
+{
+	return strcmp(s1, s2) == 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -32,11 +42,49 @@ int main(int argc, char *argv[])
 	size_t linecap = 0;
 	ssize_t linelen;
 
+	int score = 0;
+	int index = 1;
 	while ((linelen = getline(&line, &linecap, fp)) > 0) {
-		printf("%s", line);
+		int roundScore;
+		for (int i = 0; i < linelen; i++) {
+			if (line[i] == 'A' || line[i] == 'X') {
+				line[i] = 'R';
+			} else if (line[i] == 'B' || line[i] == 'Y') {
+				line[i] = 'P';
+			} else if (line[i] == 'C' || line[i] == 'Z') {
+				line[i] = 'S';
+			}
+		}
+		if (compare(line, "R R\n")) {
+			roundScore = 1 + 3;
+		} else if (compare(line, "R P\n")) {
+			roundScore = 1 + 0;
+		} else if (compare(line, "R S\n")) {
+			roundScore = 1 + 6;
+		} else if (compare(line, "P R\n")) {
+			roundScore = 2 + 6;
+		} else if (compare(line, "P P\n")) {
+			roundScore = 2 + 3;
+		} else if (compare(line, "P S\n")) {
+			roundScore = 2 + 0;
+		} else if (compare(line, "S R\n")) {
+			roundScore = 3 + 0;
+		} else if (compare(line, "S P\n")) {
+			roundScore = 3 + 6;
+		} else if (compare(line, "S S\n")) {
+			roundScore = 3 + 3;
+		} else {
+			printf("None match, '%s'", line);
+			exit(EXIT_FAILURE);
+		}
+		score += roundScore;
+		printf("index: %d\troundScore: %d\tscore: %d\tline: %s", index, roundScore, score, line);
+		index += 1;
 	}
 	free(line);
 	fclose(fp);
+
+	printf("%d\n", score);
 
 	exit(EXIT_SUCCESS);
 }
