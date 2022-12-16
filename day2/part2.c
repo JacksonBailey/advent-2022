fd137b0dfb044b9fab1ef6b4789a31b07be3617b
Jackson Bailey <jbailey@happymoney.com>
2022-12-16 15:11:33 -0500

    Day 2 solutions

--- day2/part2.c
+++ day2/part2.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-// https://adventofcode.com/2022/day/2
+// https://adventofcode.com/2022/day/2#part2
 
 FILE *open(int argc, char *argv[])
 {
@@ -23,7 +23,10 @@ FILE *open(int argc, char *argv[])
 }
 
 // a = rock, b = paper, c = scissors
-// x = rock, y = paper, z = scissors
+
+// "Anyway, the second column says how the round needs to end: X means you need
+// to lose, Y means you need to end the round in a draw, and Z means you need to
+// win. Good luck!"
 
 // core for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
 // plus the score for the outcome of the round (0 if you lost, 3 if the round
@@ -46,33 +49,24 @@ int main(int argc, char *argv[])
 	int index = 1;
 	while ((linelen = getline(&line, &linecap, fp)) > 0) {
 		int roundScore;
-		for (int i = 0; i < linelen; i++) {
-			if (line[i] == 'A' || line[i] == 'X') {
-				line[i] = 'R';
-			} else if (line[i] == 'B' || line[i] == 'Y') {
-				line[i] = 'P';
-			} else if (line[i] == 'C' || line[i] == 'Z') {
-				line[i] = 'S';
-			}
-		}
-		if (compare(line, "R R\n")) {
-			roundScore = 1 + 3;
-		} else if (compare(line, "R P\n")) {
-			roundScore = 2 + 6;
-		} else if (compare(line, "R S\n")) {
+		if (compare(line, "A X\n")) {
 			roundScore = 3 + 0;
-		} else if (compare(line, "P R\n")) {
+		} else if (compare(line, "A Y\n")) {
+			roundScore = 1 + 3;
+		} else if (compare(line, "A Z\n")) {
+			roundScore = 2 + 6;
+		} else if (compare(line, "B X\n")) {
 			roundScore = 1 + 0;
-		} else if (compare(line, "P P\n")) {
+		} else if (compare(line, "B Y\n")) {
 			roundScore = 2 + 3;
-		} else if (compare(line, "P S\n")) {
+		} else if (compare(line, "B Z\n")) {
 			roundScore = 3 + 6;
-		} else if (compare(line, "S R\n")) {
-			roundScore = 1 + 6;
-		} else if (compare(line, "S P\n")) {
+		} else if (compare(line, "C X\n")) {
 			roundScore = 2 + 0;
-		} else if (compare(line, "S S\n")) {
+		} else if (compare(line, "C Y\n")) {
 			roundScore = 3 + 3;
+		} else if (compare(line, "C Z\n")) {
+			roundScore = 1 + 6;
 		} else {
 			printf("None match, '%s'", line);
 			exit(EXIT_FAILURE);
